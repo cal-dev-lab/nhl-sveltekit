681a27ed50238751316bea8a5375ef217d6c99c2
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 00:47:39 +0100

    Added dummy text

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<svelte:head>
+	<title>Get in touch | Plantesk</title>
+	<meta name="description" content="Contact Plantesk" />
+</svelte:head>
+
+<section>
+	<div class="grid h-screen place-items-center">
+		<h1 class="text-5xl">
+			Contact
+		</h1>
+	</div>
+</section>
