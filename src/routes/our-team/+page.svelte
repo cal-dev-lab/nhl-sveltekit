ff375851173fbabcf49fa2595e2021d4058d2992
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 00:47:47 +0100

    Added dummy text

---
+++ src/routes/our-team/+page.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<svelte:head>
+	<title>The team | Plantesk</title>
+	<meta name="description" content="The team at Plantesk" />
+</svelte:head>
+
+<section>
+	<div class="grid h-screen place-items-center">
+		<h1 class="text-5xl">
+			Our Team
+		</h1>
+	</div>
+</section>
