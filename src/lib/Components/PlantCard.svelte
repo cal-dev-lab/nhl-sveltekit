0c23f50124649fd39555a580f6957d4e0de108ed
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 00:43:59 +0100

    Added props for testing

---
+++ src/lib/Components/PlantCard.svelte
@@ -0,0 +1,13 @@
+<script>
+    /*
+    *   Props to be populated in:
+    *   /lib/Components/PlantCard.svelte
+    */
+    export let title;
+    export let img;
+</script>
+
+<div>
+    <h1>{title}</h1>
+    <img {img} alt="{title}" />
+</div>
