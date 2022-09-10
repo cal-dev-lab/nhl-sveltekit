ead5ac8e189b309e639c15b6e20d80d336bb02f7
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 11:43:12 +0100

    Wrapped section in Animate

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,18 @@
+<script>
+import Animate from "../../lib/Animate.svelte";
+
+</script>
 <svelte:head>
 	<title>Our story | Plantesk</title>
 	<meta name="description" content="The story of Plantesk" />
 </svelte:head>
 
-<section>
-	<div class="grid h-screen place-items-center">
-		<h1 class="text-5xl">
-			About
-		</h1>
-	</div>
-</section>
+<Animate>
+	<section>
+		<div class="grid h-screen place-items-center">
+			<h1 class="text-5xl">
+				About
+			</h1>
+		</div>
+	</section>
+</Animate>
