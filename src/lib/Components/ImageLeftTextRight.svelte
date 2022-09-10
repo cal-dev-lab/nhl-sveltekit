df914288681ddb20ba654287704d9c7d76e3a27d
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 18:05:43 +0100

    Removed un-needed class

--- src/lib/Components/ImageLeftTextRight.svelte
+++ src/lib/Components/ImageLeftTextRight.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="max-w-7xl mx-auto py-16 px-4 sm:py-10 sm:px-6 lg:px-8">
-    <div class="grid grid-cols-reverse grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 xl:gap-x-8 mx-auto justify-center items-center bg-white p-10 rounded-3xl">
+    <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 xl:gap-x-8 mx-auto justify-center items-center bg-white p-10 rounded-3xl">
         <div class="w-full h-full z-10">
             <img 
                 class="z-10 h-full aspect-auto object-cover md:rounded-l-3xl md:rounded-tr-none rounded-t-3xl hover:scale-90 transition duration-300 md:w-full"
