fc3d00764f62644448f6d77492fcb04c36e51704
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 12:24:04 +0100

    Updated text styling

--- src/lib/Components/ImageLeftTextRight.svelte
+++ src/lib/Components/ImageLeftTextRight.svelte
@@ -7,18 +7,18 @@
 </script>
 
 <div class="max-w-7xl mx-auto py-16 px-4 sm:py-10 sm:px-6 lg:px-8">
-    <div class="grid grid-cols-reverse grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 xl:gap-x-8 mx-auto justify-center items-center">
+    <div class="grid grid-cols-reverse grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 xl:gap-x-8 mx-auto justify-center items-center bg-white p-10 rounded-3xl">
         <div class="w-full h-full z-10">
             <img 
-                class="z-10 h-full aspect-auto object-cover"
+                class="z-10 h-full aspect-auto object-cover rounded-l-3xl"
                 src={'https://plantesk.sirv.com/plantesk/ImageLeftTextRight/' + image + '.jpg'} alt="title" 
             />
         </div>
         <div>
-            <h2 class="z-50 text-5xl md:text-6xl lg:text-7xl font-grotesk font-bold">
+            <h2 class="z-50 text-5xl md:text-6xl lg:text-7xl font-grotesk font-bold text-left">
                 {title}
             </h2>
-            <p class="mt-5 font-opensans">
+            <p class="text-justify mt-5 font-opensans tracking-widest">
                 {desc}
             </p>
             <Button text="{btnText}" />
