34a006f129f04ac9e9188c135e1fc2eef0749571
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 04:23:14 +0100

    Created loader component

---
+++ src/lib/Components/Loader.svelte
@@ -0,0 +1,8 @@
+<div id="load" class="grid h-screen items-center place-items-center gap-y-1 mx-auto">
+    <img src="/tail-spin.svg" alt="Loader icon" />
+        <h1 class="font-grotesk font-bold text-6xl">
+        Loading...
+            <br />
+        <span class="text-center text-lg font-opensans">Please wait...</span>
+    </h1>
+</div>
