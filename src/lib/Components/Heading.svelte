a88571c277e9dc0778163b5ba52df7a65b4ee051
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 18:05:27 +0100

    Updated text position

--- src/lib/Components/Heading.svelte
+++ src/lib/Components/Heading.svelte
@@ -2,6 +2,6 @@
     export let title;
 </script>
 
-<div class="mx-auto md:text-center py-10">
+<div class="mx-auto text-center py-10">
     <h2 class="text-5xl font-grotesk font-bold">{title}</h2>
 </div>
