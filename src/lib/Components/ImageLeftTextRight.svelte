4012b93694ee1ea7cc30430122836954ca98792e
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 12:42:11 +0100

    Added text and image classes

--- src/lib/Components/ImageLeftTextRight.svelte
+++ src/lib/Components/ImageLeftTextRight.svelte
@@ -10,12 +10,12 @@
     <div class="grid grid-cols-reverse grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 xl:gap-x-8 mx-auto justify-center items-center bg-white p-10 rounded-3xl">
         <div class="w-full h-full z-10">
             <img 
-                class="z-10 h-full aspect-auto object-cover rounded-l-3xl"
+                class="z-10 h-full aspect-auto object-cover md:rounded-l-3xl md:rounded-tr-none rounded-t-3xl hover:scale-90 transition duration-300"
                 src={'https://plantesk.sirv.com/plantesk/ImageLeftTextRight/' + image + '.jpg'} alt="title" 
             />
         </div>
         <div>
-            <h2 class="z-50 text-5xl md:text-6xl lg:text-7xl font-grotesk font-bold text-left">
+            <h2 class="z-50 text-5xl md:text-6xl lg:text-7xl font-grotesk font-bold text-center md:text-left">
                 {title}
             </h2>
             <p class="text-justify mt-5 font-opensans tracking-widest">
