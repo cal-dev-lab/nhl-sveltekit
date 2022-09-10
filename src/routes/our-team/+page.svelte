67cf4825841cdccd4b50e636b120c8dadfe0ce94
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 21:00:36 +0100

    Updated SEO canonical

--- src/routes/our-team/+page.svelte
+++ src/routes/our-team/+page.svelte
@@ -7,7 +7,7 @@
 <svelte:head>
 	<title>The team | Plantesk</title>
 	<meta name="description" content="The team at Plantesk" />
-	<link rel="canonical" href="https://plantesk.com/our-team/" />
+	<link rel="canonical" href="https://plantesk.netlify.app/our-team/" />
 </svelte:head>
 
 <Animate>
