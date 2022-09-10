ee0a64eaeb2aff8aa933ac532615c0e588bb17d7
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 12:24:53 +0100

    Created Growing.svelte

---
+++ src/lib/Home/Growing/Growing.svelte
@@ -0,0 +1,15 @@
+<script>
+    import ImageRightTextLeft from "../../Components/ImageRightTextLeft.svelte";
+    import FromRight from "../../Components/ScrollAnimations/FromRight.svelte";
+</script>
+
+<FromRight>
+    <section>
+        <ImageRightTextLeft 
+            image="growing" 
+            title="What do house plants need to grow?"
+            desc="Depending on how they're grown and changes in plant growth through the seasons. It's best to water on an as-needed basis rather than by a set calendar schedule. In general, plants grown in well-drained soil in an appropriate-size container should be watered when the top 1/2 to 1 inch of soil feels dry. Cacti and succulents need less water; flowering plants usually need slightly more. Overwatering is one of the most common causes of houseplant death. If you're not sure how much to water, it's better to err on the dry side than to give your plants too much moisture."
+            btnText="Read more &rarr;"
+        />
+    </section>
+</FromRight>
