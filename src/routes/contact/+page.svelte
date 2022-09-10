592b9beaa2f64cad162f9a3116f179b6a0e6b2c0
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 11:43:24 +0100

    Wrapped section in animate

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,14 +1,19 @@
-<script></script>
+<script>
+import Animate from "../../lib/Animate.svelte";
+
+</script>
 
 <svelte:head>
 	<title>Get in touch | Plantesk</title>
 	<meta name="description" content="Contact Plantesk" />
 </svelte:head>
 
-<section>
-	<div class="grid h-screen place-items-center">
-		<h1 class="text-5xl">
-			Contact
-		</h1>
-	</div>
-</section>
+<Animate>
+	<section>
+		<div class="grid h-screen place-items-center">
+			<h1 class="text-5xl">
+				Contact
+			</h1>
+		</div>
+	</section>
+</Animate>
