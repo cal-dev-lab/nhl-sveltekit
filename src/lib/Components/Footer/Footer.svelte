656cb26683564f4d26464a9f28e83c62ffc4b025
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 18:05:18 +0100

    Created Footer

---
+++ src/lib/Components/Footer/Footer.svelte
@@ -0,0 +1,81 @@
+<script>
+import Heading from "../Heading.svelte";
+
+</script>
+
+<section>
+    <div class="w-screen h-45 py-10 bg-white rounded-t-3xl shadow-t-2xl">
+        <div class="max-w-7xl mx-auto">
+            <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 md:grid-cols-3 gap-x-20 xl:gap-x-8 mx-auto justify-center items-center bg-white p-10 rounded-3xl content-center">
+                <div>
+                    <h3 class="text-xl font-grotesk font-bold mb-5 border-b-2 border-green-500">
+                        Ask a question
+                    </h3>
+                    <ul>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" class="hover:text-green-500">
+                                I need a new plant
+                            </a>
+                        </li>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" class="hover:text-green-500">
+                                How to live with a plant?
+                            </a>
+                        </li>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" class="hover:text-green-500">
+                                Are plants forever?
+                            </a>
+                        </li>
+                        <li class="font-opensans mb-3">
+                            No luck?
+                            <br />
+                            <span>
+                                <a href="/#" class="font-bold text-green-500 hover:underline">
+                                    Try our forum
+                                </a>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 class="text-xl font-grotesk font-bold mb-5 border-b-2 border-green-500">
+                        Store locator
+                    </h3>
+                    <ul>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" class="hover:text-green-500">
+                                Plantesk - Hull <br />
+                                <span class="font-bold font-grotesk">
+                                    Head office
+                                </span>
+                            </a>
+                        </li>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" class="hover:text-green-500">
+                                Plantesk - Birmingham
+                            </a>
+                        </li>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" class="hover:text-green-500">
+                                Plantesk - Scotland
+                            </a>
+                        </li>
+                        <li class="font-opensans mb-3">
+                            <a href="/#" 
+                            class="hover:text-green-500">
+                                Plantesk - London
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="flex flex-col">
+                    <h3 class="text-2xl font-grotesk font-bold border-b-2 border-green-500 pb-5 mb-5">Plantesk</h3>
+                    <p class="font-opensans tracking-wider font-semibold">
+                        Plantesk reserves all rights to media and content 2022.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
