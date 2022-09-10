96ea7e5ace66c815dd38c3ba294a606643c82627
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 12:24:42 +0100

    Created FromRight animation

---
+++ src/lib/Components/ScrollAnimations/FromRight.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Saos from "saos";
+</script>
+
+<Saos animation={'from-right 2s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
+<slot></slot>
+</Saos>
+
+<style>
+    @keyframes -global-from-right {
+        0% {
+            transform: rotateX(50deg) translateX(200vw) skewX(-50deg);
+            opacity: 1;
+        }
+        100% {
+            transform: rotateX(0deg) translateX(0) skewX(0deg);
+            opacity: 1;
+        }
+    }
+</style>
