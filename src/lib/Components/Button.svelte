a0ddbb7d4c0041a3bbd7bd041005b6fe6a573b22
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 11:41:38 +0100

    Created button component with props

---
+++ src/lib/Components/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let text;
+</script>
+
+<button class="px-4 py-2 bg-green-50 text-green-600 rounded-lg mt-5 hover:bg-green-600 hover:text-green-50 transition duration-300">
+    {text}
+</button>
