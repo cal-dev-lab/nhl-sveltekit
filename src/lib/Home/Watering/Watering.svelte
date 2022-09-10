f480a752699ae0387d0ece87455125c03df7498f
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 12:42:29 +0100

    Updated title prop value

--- src/lib/Home/Watering/Watering.svelte
+++ src/lib/Home/Watering/Watering.svelte
@@ -7,7 +7,7 @@
     <section>
         <ImageLeftTextRight 
             image="watering" 
-            title="All houseplants have slightly different watering requirements"
+            title="When to water your house plant"
             desc="Depending on how they're grown and changes in plant growth through the seasons. It's best to water on an as-needed basis rather than by a set calendar schedule. In general, plants grown in well-drained soil in an appropriate-size container should be watered when the top 1/2 to 1 inch of soil feels dry. Cacti and succulents need less water; flowering plants usually need slightly more. Overwatering is one of the most common causes of houseplant death. If you're not sure how much to water, it's better to err on the dry side than to give your plants too much moisture."
             btnText="Read more &rarr;"
         />
