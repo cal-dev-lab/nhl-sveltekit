33838d02434119c1cfb2d2d2f104127d2454554d
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 18:41:53 +0100

    Added mx-auto to img

--- src/lib/Components/PersonCard.svelte
+++ src/lib/Components/PersonCard.svelte
@@ -7,7 +7,7 @@
 
 <div class="p-10 bg-white flex flex-col mx-auto justify-center rounded-3xl shadow-md hover:scale-110 transition duration-300">
     <div class="w-full">
-        <img src={'https://plantesk.sirv.com/plantesk/team/' + name + '.jpg'} class="h-64 w-64 rounded-full object-cover" alt="{name}" />
+        <img src={'https://plantesk.sirv.com/plantesk/team/' + name + '.jpg'} class="mx-auto h-64 w-64 rounded-full object-cover" alt="{name}" />
     </div>
     <div>
         <h3 class="mb-5 mt-10 font-grotesk font-bold text-3xl">{name}</h3>
@@ -18,7 +18,6 @@
                 <p class="text-xl font-grotesk">Favourite plant:</p>
                 <p class="font-opensans">{favPlant}</p>
             </div>
-            
         </div>
     </div>
 </div>
