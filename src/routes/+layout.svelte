0365348653c1c1b74f472412a6c673acc3f9adaf
Callum Lees <dev.cal.lees@gmail.com>
2022-09-10 04:01:21 +0100

    Added page loading animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,31 @@
 <script>
 	import Navbar from '$lib/Navbar/Navbar.svelte';
 	import '../app.css';
+	import { onMount } from "svelte";
+
+	// For page loading
+	let isPageLoaded = false;
+
+	onMount(() => {
+
+		isPageLoaded = true;
+
+		if ( isPageLoaded ) {
+			console.log("Page has loaded successfully!");
+		} else {
+			console.log("Oh no, something has gone wrong here...");
+		}
+		
+	});
 </script>
 
 <Navbar />
+
+{#if !isPageLoaded}
+	<div id="load" class="grid h-screen items-center place-items-center mx-auto">
+		<img src="/tail-spin.svg" alt="Loader icon" />
+	</div>
+{/if}
 
 <main>
 	<slot />
